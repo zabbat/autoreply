a470bbf137cd2124c72dccfedd9861d8901b0415
Your Name <johan.jungbeck@wandroid.net>
2017-05-30 14:13:42 +0100

    Add test for jenkins
    add a smaple test for jenkins

---
+++ app/src/test/java/net/wandroid/answer/JenkinsTest.java
@@ -0,0 +1,13 @@
+package net.wandroid.answer;
+
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+public class JenkinsTest {
+    @Test
+    public void test(){
+ 
+    }
+}
